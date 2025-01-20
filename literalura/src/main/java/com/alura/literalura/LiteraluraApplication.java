993fab6a07d4d59335e49eb7d974f2d9ea596d72
Betzabé Jiménez Hernández <162236694+BetzabeJimenezH@users.noreply.github.com>
2025-01-20 00:39:23 -0600

    Add files via upload

---
+++ literalura/src/main/java/com/alura/literalura/LiteraluraApplication.java
@@ -0,0 +1,26 @@
+package com.alura.literalura;
+
+import com.alura.literalura.principal.Menu;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiteraluraApplication implements CommandLineRunner {
+
+	@Autowired
+	private final Menu principal;
+
+	public LiteraluraApplication(Menu principal){
+		this.principal = principal;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiteraluraApplication.class, args);
+	}
+	@Override
+	public void run(String... args) throws Exception{
+		principal.mostrarMenu();
+	}
+}
